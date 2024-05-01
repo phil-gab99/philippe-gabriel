08baaed172a08f51be2ff42fb1d749a732eda761
Philippe Gabriel <pgabriel999@hotmail.com>
2024-04-30 23:56:58 -0400

    Started working on experience timeline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProjectsComponent } from './components/projects/projects.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, NavbarComponent, ProjectsComponent],
+  imports: [
+    ExperienceComponent,
+    HomeComponent,
+    NavbarComponent,
+    ProjectsComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
